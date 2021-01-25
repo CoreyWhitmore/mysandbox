a239fb7585e476494e9def4036eaf54b388503a6
CoreyWhitmore <cjwhit97@hotmail.com>
2021-01-25 09:31:14 -0700

    Created css override file

--- src/views/ParabolicTargeting.vue
+++ src/views/ParabolicTargeting.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="parabolictargeting">
+  <div class="parabolictargeting container-fluid">
     <div class="row">
       <div class="d-flex loading-component justify-content-center m-2">
-        <canvas id="animationCanvas" class="parabolic-targeting" style="border:1px solid #000000;">
+        <canvas id="animationCanvas" class="parabolic-targeting">
         </canvas>
       </div>
     </div>
